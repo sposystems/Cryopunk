93181682a52150ecc532279a86bcfbbe276d1fd4
sposystems <spo@sposystems.com>
2015-01-27 22:18:16 -0500

    Scene switching alpha
    Just need to set it up so that the player is not destroyed and created
    again upon scene switch.  Good enough to demo in class.

---
+++ game/Assets/Scenes/SceneScripts/SceneChanger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour {
+	public string level;
+	void OnTriggerEnter(Collider other) { 
+		Application.LoadLevel(level); 
+	}
+}
---
+++ game/Assets/Scenes/SceneScripts/worldToTownTest1.js
@@ -0,0 +1,7 @@
+﻿var myLevel : String;
+ 
+function OnCollisionEnter (myCollision : Collision) {
+ if(myCollision.gameObject.name == "Player"){
+  Application.LoadLevel(myLevel);
+ }
+}

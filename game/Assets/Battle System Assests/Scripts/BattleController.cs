742c79ace81cf294aa7dff396323347014147adc
Peter Ficorelli <petefic@gmail.com>
2015-01-29 22:06:03 -0500

    Added multiple party members and enemies
    currently super buggy and broken, should be able to get it working good
    over the weekend

--- game/Assets/Battle System Assests/Scripts/BattleController.cs
+++ game/Assets/Battle System Assests/Scripts/BattleController.cs
@@ -4,38 +4,85 @@ using System.Collections;
 public class BattleController : MonoBehaviour {
 
 	public BattleGUI gui;
-	public Character playerCharacter;
-	public Character enemyCharacter;
+	public Character player1Character;
+	public Character player2Character;
+	public Character player3Character;
+	public Character player4Character;
+	public Character player5Character;
+	public Character enemy1Character;
+	public Character enemy2Character;
+	public Character enemy3Character;
+	public Character enemy4Character;
+	public Character enemy5Character;
 	
 	private enum BattleStates {
-		PlayerTurn,
-		EnemyTurn,
+		Player1Turn,
+		Player2Turn,
+		Player3Turn,
+		Player4Turn,
+		Player5Turn,
+		Enemy1Turn,
+		Enemy2Turn,
+		Enemy3Turn,
+		Enemy4Turn,
+		Enemy5Turn,
 		WinBattle,
 		LoseBattle
 	};
 	private BattleStates currentState;
-	private int randNum;
+	private int randAbility;
+	private int randTarget;
 
+	//complete the turn and set up for next turn
 	public void EndTurn() {
 		gui.UpdateGui();
 
 		//clear all targets
-		playerCharacter.SetTargeted(false);
-		playerCharacter.SetTargetable(false);
-		enemyCharacter.SetTargeted(false);
-		enemyCharacter.SetTargetable(false);
+		player1Character.SetTargeted(false);
+		player1Character.SetTargetable(false);
+		player2Character.SetTargeted(false);
+		player2Character.SetTargetable(false);
+		player3Character.SetTargeted(false);
+		player3Character.SetTargetable(false);
+		player4Character.SetTargeted(false);
+		player4Character.SetTargetable(false);
+		player5Character.SetTargeted(false);
+		player5Character.SetTargetable(false);
+		enemy1Character.SetTargeted(false);
+		enemy1Character.SetTargetable(false);
+		enemy2Character.SetTargeted(false);
+		enemy2Character.SetTargetable(false);
+		enemy3Character.SetTargeted(false);
+		enemy3Character.SetTargetable(false);
+		enemy4Character.SetTargeted(false);
+		enemy4Character.SetTargetable(false);
+		enemy5Character.SetTargeted(false);
+		enemy5Character.SetTargetable(false);
 
-		//check for win/loss
-		if (playerCharacter.GetHP() <= 0) {
+		//check for loss
+		if (player1Character.GetHP() <= 0 && 
+		player2Character.GetHP() <= 0 && 
+		player3Character.GetHP() <= 0 && 
+		player4Character.GetHP() <= 0 && 
+		player5Character.GetHP() <= 0) {
 			currentState = BattleStates.LoseBattle;
-		} else if (enemyCharacter.GetHP() <= 0) {
+		
+		//check for win
+		} else if (enemy1Character.GetHP() <= 0 &&
+		enemy1Character.GetHP() <= 0 &&
+		enemy2Character.GetHP() <= 0 &&
+		enemy3Character.GetHP() <= 0 &&
+		enemy4Character.GetHP() <= 0 &&
+		enemy5Character.GetHP() <= 0) {
 			currentState = BattleStates.WinBattle;
-		} else if (currentState == BattleStates.PlayerTurn) {
-			currentState = BattleStates.EnemyTurn;
-		} else if (currentState == BattleStates.EnemyTurn) {
-			currentState = BattleStates.PlayerTurn;
+			
+		//if all enemies have gone, go back to player 1
+		} else if (currentState == BattleStates.Enemy5Turn) {
+			currentState = BattleStates.Player1Turn;
+			
+		//go to the next player's turn
 		} else {
-			Debug.Log("Somehow entered a bad state");
+			currentState++;
 		}
 
 		ChangeState();
@@ -44,38 +91,119 @@ public class BattleController : MonoBehaviour {
 	//setup battle
 	private void Start() {
 		gui.UpdateGui();
-		currentState = BattleStates.PlayerTurn;
+		currentState = BattleStates.Player1Turn;
 		ChangeState();
 	}
 	
 	private void ChangeState() {
 		switch(currentState) {
-		case(BattleStates.PlayerTurn):
-			//Debug.Log("Player turn state entered");
-			//enable player's abilities
-			gui.playerAbility1.interactable = true;
-			gui.playerAbility2.interactable = true;
-			break;
-		case(BattleStates.EnemyTurn):
-			//Debug.Log("enemy turn state entered");
-			//enemy chooses random attack
-			randNum = Random.Range(0,2);
-			if (randNum == 0) {
-				enemyCharacter.ability1.Use(playerCharacter);
-			} else if (randNum == 1) {
-				enemyCharacter.ability2.Use(playerCharacter);
+		
+		case(BattleStates.Player1Turn):
+			if (player1Character.GetHP() > 0) {
+				gui.player1Ability1.interactable = true;
+				gui.player1Ability2.interactable = true;
 			} else {
-				Debug.Log("ran gen error");
+				currentState++;
+				ChangeState();
 			}
 			break;
+			
+		case(BattleStates.Player2Turn):
+			if (player2Character.GetHP() > 0) {
+				gui.player2Ability1.interactable = true;
+				gui.player2Ability2.interactable = true;
+			} else {
+				currentState++;
+				ChangeState();
+			}
+			break;
+			
+		case(BattleStates.Player3Turn):
+			if (player3Character.GetHP() > 0) {
+				gui.player3Ability1.interactable = true;
+				gui.player3Ability2.interactable = true;
+			} else {
+				currentState++;
+				ChangeState();
+			}
+			break;
+			
+		case(BattleStates.Player4Turn):
+			if (player4Character.GetHP() > 0) {
+				gui.player4Ability1.interactable = true;
+				gui.player4Ability2.interactable = true;
+			} else {
+				currentState++;
+				ChangeState();
+			}
+			break;
+			
+		case(BattleStates.Player5Turn):
+			if (player5Character.GetHP() > 0) {
+				gui.player5Ability1.interactable = true;
+				gui.player5Ability2.interactable = true;
+			} else {
+				currentState++;
+				ChangeState();
+			}
+			break;
+
+		case(BattleStates.Enemy1Turn):
+			EnemyTurn(enemy1Character);
+			break;
+			
+		case(BattleStates.Enemy2Turn):
+			EnemyTurn(enemy2Character);
+			break;
+			
+		case(BattleStates.Enemy3Turn):
+			EnemyTurn(enemy3Character);
+			break;
+			
+		case(BattleStates.Enemy4Turn):
+			EnemyTurn(enemy4Character);
+			break;
+			
+		case(BattleStates.Enemy5Turn):
+			EnemyTurn(enemy5Character);
+			break;
+			
 		case(BattleStates.WinBattle):
 			//display win
 			Debug.Log("win state entered");
 			break;
+			
 		case(BattleStates.LoseBattle):
 			//display game over
 			Debug.Log("lose state entered");
 			break;
+		}
+	}
+	
+	//enemy chooses random attack
+	private void EnemyTurn(Character enemy) {
+		randAbility = Random.Range(0,2);
+		randTarget = Random.Range(0,5);
+		Character target = player1Character;
+		
+		if (randTarget == 0) {
+			target = player1Character;
+		} else if (randTarget == 1) {
+			target = player2Character;
+		} else if (randTarget == 2) {
+			target = player3Character;
+		} else if (randTarget == 3) {
+			target = player4Character;
+		} else if (randTarget == 4) {
+			target = player5Character;
+		} else {
+			Debug.Log("ERROR");
+		}
+		
+		if (randAbility == 0) {
+			enemy.ability1.Use(target);
+		} else if (randAbility == 1) {
+			enemy.ability2.Use(target);
 		}
 	}
 }
--- game/Assets/Battle System Assests/Scripts/BattleGUI.cs
+++ game/Assets/Battle System Assests/Scripts/BattleGUI.cs
@@ -4,35 +4,130 @@ using System.Collections;
 
 public class BattleGUI : MonoBehaviour {
 
-	public Text playerName;
-	public Text playerHP;
-	public Text playerSP;
-	public Button playerAbility1;
-	public Button playerAbility2;
-	public Text enemyName;
-	public Text enemyHP;
+	public Text player1Name;
+	public Text player1HP;
+	public Text player1SP;
+	public Button player1Ability1;
+	public Button player1Ability2;
+	public Text player2Name;
+	public Text player2HP;
+	public Text player2SP;
+	public Button player2Ability1;
+	public Button player2Ability2;
+	public Text player3Name;
+	public Text player3HP;
+	public Text player3SP;
+	public Button player3Ability1;
+	public Button player3Ability2;
+	public Text player4Name;
+	public Text player4HP;
+	public Text player4SP;
+	public Button player4Ability1;
+	public Button player4Ability2;
+	public Text player5Name;
+	public Text player5HP;
+	public Text player5SP;
+	public Button player5Ability1;
+	public Button player5Ability2;
+	public Text enemy1Name;
+	public Text enemy1HP;
+	public Text enemy2Name;
+	public Text enemy2HP;
+	public Text enemy3Name;
+	public Text enemy3HP;
+	public Text enemy4Name;
+	public Text enemy4HP;
+	public Text enemy5Name;
+	public Text enemy5HP;
 
-	private GameObject player;
-	private Character playerCharacter;
-	private GameObject enemy;
-	private Character enemyCharacter;
+	private GameObject player1;
+	private Character player1Character;
+	private GameObject player2;
+	private Character player2Character;
+	private GameObject player3;
+	private Character player3Character;
+	private GameObject player4;
+	private Character player4Character;
+	private GameObject player5;
+	private Character player5Character;
+	private GameObject enemy1;
+	private Character enemy1Character;
+	private GameObject enemy2;
+	private Character enemy2Character;
+	private GameObject enemy3;
+	private Character enemy3Character;
+	private GameObject enemy4;
+	private Character enemy4Character;
+	private GameObject enemy5;
+	private Character enemy5Character;
 	
 	public void UpdateGui() {
-		//Debug.Log("gui updated");
-		playerName.text = playerCharacter.characterName;
-		playerHP.text = "HP: " + playerCharacter.GetHP() + "/" + playerCharacter.hp;
-		playerSP.text = "SP: " + playerCharacter.GetSP() + "/" + playerCharacter.sp;
+		player1Name.text = player1Character.characterName;
+		player1HP.text = "HP: " + player1Character.GetHP() + "/" + player1Character.hp;
+		player1SP.text = "SP: " + player1Character.GetSP() + "/" + player1Character.sp;
 		
-		enemyName.text = enemyCharacter.characterName;
-		enemyHP.text = "HP: " + enemyCharacter.GetHP() + "/" + enemyCharacter.hp;
+		player2Name.text = player2Character.characterName;
+		player2HP.text = "HP: " + player2Character.GetHP() + "/" + player2Character.hp;
+		player2SP.text = "SP: " + player2Character.GetSP() + "/" + player2Character.sp;
+		
+		player3Name.text = player3Character.characterName;
+		player3HP.text = "HP: " + player3Character.GetHP() + "/" + player3Character.hp;
+		player3SP.text = "SP: " + player3Character.GetSP() + "/" + player3Character.sp;
+		
+		player4Name.text = player4Character.characterName;
+		player4HP.text = "HP: " + player4Character.GetHP() + "/" + player4Character.hp;
+		player4SP.text = "SP: " + player4Character.GetSP() + "/" + player4Character.sp;
+		
+		player5Name.text = player5Character.characterName;
+		player5HP.text = "HP: " + player5Character.GetHP() + "/" + player5Character.hp;
+		player5SP.text = "SP: " + player5Character.GetSP() + "/" + player5Character.sp;
+		
+		enemy1Name.text = enemy1Character.characterName;
+		enemy1HP.text = "HP: " + enemy1Character.GetHP() + "/" + enemy1Character.hp;
+		
+		enemy2Name.text = enemy2Character.characterName;
+		enemy2HP.text = "HP: " + enemy2Character.GetHP() + "/" + enemy2Character.hp;
+		
+		enemy3Name.text = enemy3Character.characterName;
+		enemy3HP.text = "HP: " + enemy3Character.GetHP() + "/" + enemy3Character.hp;
+		
+		enemy4Name.text = enemy4Character.characterName;
+		enemy4HP.text = "HP: " + enemy4Character.GetHP() + "/" + enemy4Character.hp;
+		
+		enemy5Name.text = enemy5Character.characterName;
+		enemy5HP.text = "HP: " + enemy5Character.GetHP() + "/" + enemy5Character.hp;
 	}
 
 	//called on object creation, sets up references
 	private void Awake() {
-		player = GameObject.FindGameObjectWithTag("Player");
-		playerCharacter = player.GetComponent<Character>();
+		player1 = GameObject.FindGameObjectWithTag("Player");
+		player1Character = player1.GetComponent<Character>();
+		
+		player2 = GameObject.FindGameObjectWithTag("Player");
+		player2Character = player2.GetComponent<Character>();
+		
+		player3 = GameObject.FindGameObjectWithTag("Player");
+		player3Character = player3.GetComponent<Character>();
+		
+		player4 = GameObject.FindGameObjectWithTag("Player");
+		player4Character = player4.GetComponent<Character>();
+		
+		player5 = GameObject.FindGameObjectWithTag("Player");
+		player5Character = player5.GetComponent<Character>();
 
-		enemy = GameObject.FindGameObjectWithTag("Enemy");
-		enemyCharacter = enemy.GetComponent<Character>();
+		enemy1 = GameObject.FindGameObjectWithTag("Enemy");
+		enemy1Character = enemy1.GetComponent<Character>();
+		
+		enemy2 = GameObject.FindGameObjectWithTag("Enemy");
+		enemy2Character = enemy2.GetComponent<Character>();
+		
+		enemy3 = GameObject.FindGameObjectWithTag("Enemy");
+		enemy3Character = enemy3.GetComponent<Character>();
+		
+		enemy4 = GameObject.FindGameObjectWithTag("Enemy");
+		enemy4Character = enemy4.GetComponent<Character>();
+		
+		enemy5 = GameObject.FindGameObjectWithTag("Enemy");
+		enemy5Character = enemy5.GetComponent<Character>();
 	}
 }

cb77b90e5393f654379559cfac75d2f686533b83
mdpulis <pulis.michael@gmail.com>
2015-02-16 13:56:37 -0500

    Main Menu
    Update with the Main Menu and some more assets

---
+++ game/Assets/scripts/UI_Scripts/MainMenu_UI/mainMenuButtons.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenuButtons : MonoBehaviour {
+
+	public void LoadScene(){
+		Application.LoadLevel ("townTest1");
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+
+
+}

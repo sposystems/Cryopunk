422928fa4ec949e8ae94f4d5bcbfcc7a2bc49418
sposystems <spo@sposystems.com>
2015-02-21 19:46:44 -0500

    Added initial values

--- game/Assets/scripts/BattleLauncher.cs
+++ game/Assets/scripts/BattleLauncher.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class BattleLauncher : MonoBehaviour {
 
-	public static int enemyType;
-	public static int enemyQuantity;
-	public static bool fifthMember;
-	public static int location;
+	public static int enemyType = 0;
+	public static int enemyQuantity = 0;
+	public static bool fifthMember = false;
+	public static int location = 0;
 	public int eType, eQuantity, eLocation;
 	public bool fMember;
 	public string level;

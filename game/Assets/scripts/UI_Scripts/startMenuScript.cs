46d1cd294aba5022461714045b1dc2e0db621f76
sposystems <spo@sposystems.com>
2015-03-23 13:58:18 -0400

    housecleaning

--- game/Assets/scripts/UI_Scripts/startMenuScript.cs
+++ game/Assets/scripts/UI_Scripts/startMenuScript.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class startMenuScript : MonoBehaviour {
-	public CanvasGroup startMenuCanvas;
+	//public CanvasGroup startMenuCanvas;
 	public static float deltaTime;
 	private static float _lastframetime;
 
@@ -11,14 +11,53 @@ public class startMenuScript : MonoBehaviour {
 	private bool showGUI = false;
 
 	void Start () {
-		_lastframetime = Time.realtimeSinceStartup;
+		//_lastframetime = Time.realtimeSinceStartup;
 	}
 
+	void FixedUpdate(){
+		//Press T or Tab to pause the game
+
+	}
+
+
+
+
+
+	/*
 	// Update is called once per frame
 	void Update () {
-		deltaTime = Time.realtimeSinceStartup - _lastframetime;
-		_lastframetime = Time.realtimeSinceStartup;
+		//deltaTime = Time.realtimeSinceStartup - _lastframetime;
+		//_lastframetime = Time.realtimeSinceStartup;
+		if ((Input.GetKeyDown(KeyCode.T) || Input.GetKeyDown(KeyCode.Tab)) && !pauseGame) {
+			//If not currently paused, then stop time and show the GUI
+			if (!pauseGame) {
+				Debug.Log("Tab/T properly pressed to pause game.");
+				pauseGame = true;
+				//StartCoroutine(Wait(.05f)); //IEnumerator Wait, then stops
+				//Time.timeScale = 0;
+				startMenuCanvas.animation.Play ("pause_menu_anim1");
+			}
+		}else if ((Input.GetKeyDown (KeyCode.T) || Input.GetKeyDown (KeyCode.Tab)) && pauseGame) {
+			if (pauseGame) {
+				Debug.Log ("Tab/T properly pressed to UNpause game.");
+				//Time.timeScale = 1;
+				pauseGame = false;
+				startMenuCanvas.animation.Play ("pause_menu_anim1_exit");
+			}
+		}
 
+	}
+	*/
+
+	IEnumerator Wait(float seconds){
+		Debug.Log ("Waited for " + seconds + " seconds.");
+		pauseGame = true;
+		yield return new WaitForSeconds(seconds);
+		Time.timeScale = 0;
+	}
+
+	/*
+	 * void FixedUpdate(){
 		//Press T or Tab to pause the game
 		if (Input.GetKeyDown(KeyCode.T) || Input.GetKeyDown(KeyCode.Tab)) {
 			//If not currently paused, then stop time and show the GUI
@@ -26,7 +65,8 @@ public class startMenuScript : MonoBehaviour {
 				Debug.Log("Tab/T properly pressed to pause game.");
 				pauseGame = true;
 				showGUI = true;
-				StartCoroutine(Wait(.333f)); //IEnumerator Wait, then stops
+				//StartCoroutine(Wait(.333f)); //IEnumerator Wait, then stops
+				Time.timeScale = 0;
 			}else if (pauseGame){
 				Debug.Log("Tab/T properly pressed to UNpause game.");
 				Time.timeScale = 1;
@@ -34,19 +74,15 @@ public class startMenuScript : MonoBehaviour {
 				showGUI = false;
 			}
 
-			//If we are to show the GUI, play the start animation; otherwise, the exit one
 			if (showGUI == true) {
 				startMenuCanvas.animation.Play ("pause_menu_anim1");
 			} else {
 				startMenuCanvas.animation.Play ("pause_menu_anim1_exit");
 			}
-		}
-	}
+			
+			//If we are to show the GUI, play the start animation; otherwise, the exit one
 
-	IEnumerator Wait(float seconds){
-		Debug.Log ("Waited for " + seconds + " seconds.");
-		yield return new WaitForSeconds(seconds);
-		Time.timeScale = 0;
-	}
+		}
+	}*/
 	
 }

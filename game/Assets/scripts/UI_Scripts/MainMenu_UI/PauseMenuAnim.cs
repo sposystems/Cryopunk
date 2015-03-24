0011b7f7e9717643c540514bd162de1a6448b0c4
mdpulis <pulis.michael@gmail.com>
2015-03-24 00:05:25 -0400

    TONS OF MENU WORK
    MEGA DONE (finished the items and characters screens with awesome
    animations)

--- game/Assets/scripts/UI_Scripts/MainMenu_UI/PauseMenuAnim.cs
+++ game/Assets/scripts/UI_Scripts/MainMenu_UI/PauseMenuAnim.cs
@@ -1,72 +1,279 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PauseMenuAnim : MonoBehaviour {
 
+	//startMenuCanvas is in charge of all main commands in this script
 	public CanvasGroup startMenuCanvas;
+	//pauseGame keeps track of pause state for pausing/unpausing
 	private bool pauseGame = false;
-	Animator anim;
+	//inCharScreen used for Character Screen loops
+	private bool inCharScreen = false;
+	//anim allows us to animate without needing to use the Animator for initial pausing
+	private Animator anim;
 
+	//HP Text, SP, LV, etc. for all 5 characters' boxes
+	private Text hpTextK;
+	private Text hpTextL;
+	private Text hpTextR;
+	private Text hpTextC;
+	private Text hpTextS;
+
+	private Text spTextK;
+	private Text spTextL;
+	private Text spTextR;
+	private Text spTextC;
+	private Text spTextS;
+
+	private Text lvTextK;
+	private Text lvTextL;
+	private Text lvTextR;
+	private Text lvTextC;
+	private Text lvTextS;
+
+	private Text expTextK;
+	private Text expTextL;
+	private Text expTextR;
+	private Text expTextC;
+	private Text expTextS;
+
+	//Items Screen Variables
+	private Text hpPotions;
+	private Text spPotions;
+
+	private Sprite hpPotionArt;
+	private Sprite spPotionArt;
+
+	private Image itemImage;
+	private Text itemDesc;
+	/// <summary>
+	/// INSERT OTHER ITEMS HERE
+	/// </summary>
+
+	
+	//Character Screen Variables
+	private Text strStat;
+	private Text defStat;
+	private Text magStat;
+	private Text hpStatChars;
+	private Text spStatChars;
+	private Image hpBarChars;
+	private Image spBarChars;
+	private Text lvStat;
+	private Text expStat;
+	private Text charName;
+	private Image characterImage;
+	private Image characterImageShadow;
+	private Text charBookDialog;
+
+	private Sprite kira_art;
+	private Sprite law_art;
+	private Sprite robo_art;
+	private Sprite constance_art;
+	private Sprite solan_art;
+
+	//Assign all variables to their respective components
 	public void Start(){
 		anim = startMenuCanvas.GetComponent<Animator> ();
 
-	}
-	/*
-	public void goToItems(){
-		startMenuCanvas.animation.Play ("pause_menu_anim_main_exit");
-		StartCoroutine (Wait (.33f));
-		//startMenuCanvas.animation.Play ("pause_menu_items_enter");
+		hpPotions = GameObject.Find ("Health Potion Quantity").GetComponent<Text>();
+		spPotions = GameObject.Find ("Special Potion Quantity").GetComponent<Text>();
+		itemDesc = GameObject.Find ("Item Description").GetComponent<Text>();
+		itemImage = GameObject.Find ("Item Image Big").GetComponent<Image>();
+
+		hpPotionArt = Resources.Load<Sprite> ("hp_potion");
+		spPotionArt = Resources.Load<Sprite> ("sp_potion");
+
+		strStat = GameObject.Find ("STR Stat").GetComponent<Text>();
+		defStat = GameObject.Find ("DEF Stat").GetComponent<Text>();
+		magStat = GameObject.Find ("MAG Stat").GetComponent<Text>();
+		hpStatChars = GameObject.Find ("HP_Num (Chars)").GetComponent<Text>();
+		spStatChars = GameObject.Find ("SP_Num (Chars)").GetComponent<Text>();
+		hpBarChars = GameObject.Find ("hp_bar_percent (Chars)").GetComponent<Image>();
+		spBarChars = GameObject.Find ("sp_bar_percent (Chars)").GetComponent<Image>();
+		lvStat = GameObject.Find ("Level Stat (Chars)").GetComponent<Text>();
+		expStat = GameObject.Find ("To Next Stat (Chars)").GetComponent<Text>();
+		charName = GameObject.Find ("Char Name Text").GetComponent<Text>();
+		characterImage = GameObject.Find ("Char Image").GetComponent<Image>();
+		characterImageShadow = GameObject.Find ("Char Image Shadow").GetComponent<Image>();
+		charBookDialog = GameObject.Find ("Char Book Dialog").GetComponent<Text> ();
+
+		kira_art = Resources.Load<Sprite> ("kira_tp");
+		law_art = Resources.Load<Sprite> ("law_tp");
+		robo_art = Resources.Load<Sprite> ("robo_tp");
+		constance_art = Resources.Load<Sprite> ("constance_tp");
+		solan_art = Resources.Load<Sprite> ("solan_tp");
+
+
 	}
 
-	public void itemsGoBackToMain(){
-		startMenuCanvas.animation.Play ("pause_menu_items_exit");
-		StartCoroutine (Wait (.33f));
-		startMenuCanvas.animation.Play ("pause_menu_anim1");
-	}
-	
+	/*
 	IEnumerator Wait(float seconds){
 		Debug.Log ("Waited for " + seconds + " seconds.");
 		yield return new WaitForSeconds(seconds);
 	}
-
-	public void Shift1(){
-		anim.SetBool ("mainState", true);
-		anim.SetTrigger ("mainState");
-	}
 	*/
-	
-	public void OpenMenu(){
-		if ((Input.GetKeyDown (KeyCode.T) || Input.GetKeyDown (KeyCode.Tab)) && !pauseGame) {
-			//If not currently paused, then stop time and show the GUI
-			if (!pauseGame) {
-				Debug.Log ("Tab/T properly pressed to pause game.");
-				pauseGame = true;
-				//StartCoroutine(Wait(.05f)); //IEnumerator Wait, then stops
-				//Time.timeScale = 0;
-				//startMenuCanvas.animation.Play ("pause_menu_anim1");
-			}
-		} else if ((Input.GetKeyDown (KeyCode.T) || Input.GetKeyDown (KeyCode.Tab)) && pauseGame) {
-			if (pauseGame) {
-				Debug.Log ("Tab/T properly pressed to UNpause game.");
-				//Time.timeScale = 1;
-				pauseGame = false;
-				//startMenuCanvas.animation.Play ("pause_menu_anim1_exit");
-			}
+
+	public void hpPotionPress(){
+		changeItemInfo ("hpPotion");
+	}
+
+	public void spPotionPress(){
+		changeItemInfo ("spPotion");
+	}
+
+	private void changeItemInfo(string itemName){
+		if(itemName == "hpPotion"){
+			hpPotions.text = "x" + "12"; //CHANGE TO ACTUAL NUMBER FROM DB
+			itemDesc.text = "A restorative potion used for restoring a small amount of HP.";
+			itemImage.GetComponent<Image>().sprite = hpPotionArt;
+		}else if(itemName == "spPotion"){
+			spPotions.text = "x" + "4";
+			itemDesc.text = "A special potion used for restoring a small amount of SP.";
+			itemImage.GetComponent<Image>().sprite = spPotionArt;
 		}
 	}
 
 
 
+	private void getKiraInfo(){
+
+	}
+
+	public void KiraPress(){
+		charName.text = "Kira";
+		strStat.text = "300"; //ASSIGN THESE VARIABLES ACCORDING TO DATABASE DATA
+		defStat.text = "-50"; //THEY MUST USE MATH VALUES IN FINAL VERSION
+		magStat.text = "100";
+		lvStat.text = "LV " + "55";
+		expStat.text = "423";
+		hpStatChars.text = "200/400";
+		spStatChars.text = "15/30";
+		hpBarChars.fillAmount = 0.5f;
+		spBarChars.fillAmount = 0.5f;
+		charBookDialog.text = "\"What kind of jokers are you all anyways?\"";
+
+		characterImage.GetComponent<Image>().sprite = kira_art;
+		characterImageShadow.GetComponent<Image>().sprite = kira_art;
+
+		GoToCharacters ();
+	}
+
+	public void LawPress(){
+		charName.text = "Law";
+		strStat.text = "1"; //ASSIGN THESE VARIABLES ACCORDING TO DATABASE DATA
+		defStat.text = "-9999999"; //THEY MUST USE MATH VALUES IN FINAL VERSION
+		magStat.text = "magic?";
+		lvStat.text = "LV " + "1";
+		expStat.text = ".76";
+		hpStatChars.text = "1/100";
+		spStatChars.text = "0/0";
+		hpBarChars.fillAmount = 0.01f;
+		spBarChars.fillAmount = 0.0f;
+		charBookDialog.text = "\"Wait, you don't expect a lawyer to fight, do you?  I object to such nonsense!\"";
+		
+		characterImage.GetComponent<Image>().sprite = law_art;
+		characterImageShadow.GetComponent<Image>().sprite = law_art;
+		
+		GoToCharacters ();
+	}
+
+	public void RoboPress(){
+		charName.text = "Robo";
+		strStat.text = "1000"; //ASSIGN THESE VARIABLES ACCORDING TO DATABASE DATA
+		defStat.text = "250"; //THEY MUST USE MATH VALUES IN FINAL VERSION
+		magStat.text = "50";
+		lvStat.text = "LV " + "76";
+		expStat.text = "7382";
+		hpStatChars.text = "1000/1000";
+		spStatChars.text = "25/25";
+		hpBarChars.fillAmount = 1.0f;
+		spBarChars.fillAmount = 1.0f;
+		charBookDialog.text = "\"BEEP BOOP BOOP BEEP zzt\"";
+		
+		characterImage.GetComponent<Image>().sprite = robo_art;
+		characterImageShadow.GetComponent<Image>().sprite = robo_art;
+		
+		GoToCharacters ();
+	}
+
+	public void ConstancePress(){
+		charName.text = "Constance";
+		strStat.text = "$8.95"; //ASSIGN THESE VARIABLES ACCORDING TO DATABASE DATA
+		defStat.text = "5"; //THEY MUST USE MATH VALUES IN FINAL VERSION
+		magStat.text = "1337";
+		lvStat.text = "LV " + "12";
+		expStat.text = "423";
+		hpStatChars.text = "75/300";
+		spStatChars.text = "700/700";
+		hpBarChars.fillAmount = 0.25f;
+		spBarChars.fillAmount = 1.0f;
+		charBookDialog.text = "\"Just one more battle... I will prevail!\"";
+		
+		characterImage.GetComponent<Image>().sprite = constance_art;
+		characterImageShadow.GetComponent<Image>().sprite = constance_art;
+		
+		GoToCharacters ();
+	}
 	
+	public void SolanPress(){
+		charName.text = "Solan";
+		strStat.text = "69"; //ASSIGN THESE VARIABLES ACCORDING TO DATABASE DATA
+		defStat.text = "42"; //THEY MUST USE MATH VALUES IN FINAL VERSION
+		magStat.text = "777";
+		lvStat.text = "LV " + "95";
+		expStat.text = "60023";
+		hpStatChars.text = "495/500";
+		spStatChars.text = "200/1000";
+		hpBarChars.fillAmount = 0.99f;
+		spBarChars.fillAmount = 0.2f;
+		charBookDialog.text = "\"Y'all are idiots.\"";
+		
+		characterImage.GetComponent<Image>().sprite = solan_art;
+		characterImageShadow.GetComponent<Image>().sprite = solan_art;
+		
+		GoToCharacters ();
+	}
+
+
+	public void GoToItems(){
+		anim.SetBool ("Items Screen", true);
+	}
+
+	public void GoToCharacters(){
+		if (!inCharScreen) {
+			inCharScreen = true;
+			anim.SetBool ("Characters Screen", true);
+		} else {
+			anim.SetTrigger ("Reenter");
+		}
+	}
+	
+	public void GoToOptions(){
+		anim.SetBool ("Options Screen", true);
+	}
+
+	public void GoToExit(){
+		anim.SetBool ("Exit Screen", true);
+	}
+
+	public void GoToMain(){
+		inCharScreen = false;
+		anim.SetBool ("Items Screen", false);
+		anim.SetBool ("Characters Screen", false);
+		anim.SetBool ("Options Screen", false);
+		anim.SetBool ("Exit Screen", false);
+	}
+
+
 	public void Update(){
 		if ((Input.GetKeyDown (KeyCode.T) || Input.GetKeyDown (KeyCode.Tab)) && !pauseGame) {
 			//If not currently paused, then stop time and show the GUI
 			if (!pauseGame) {
 				Debug.Log ("Tab/T properly pressed to pause game.");
 				pauseGame = true;
-				//StartCoroutine(Wait(.05f)); //IEnumerator Wait, then stops
 				Time.timeScale = 0;
-				//startMenuCanvas.animation.Play ("pause_menu_anim1");
 				anim.SetBool ("Main Screen", true);
 			}
 		} else if ((Input.GetKeyDown (KeyCode.T) || Input.GetKeyDown (KeyCode.Tab)) && pauseGame) {
@@ -74,8 +281,13 @@ public class PauseMenuAnim : MonoBehaviour {
 				Debug.Log ("Tab/T properly pressed to UNpause game.");
 				Time.timeScale = 1;
 				pauseGame = false;
-				//startMenuCanvas.animation.Play ("pause_menu_anim1_exit");
+
+				inCharScreen = false;
 				anim.SetBool ("Main Screen", false);
+				anim.SetBool ("Items Screen", false);
+				anim.SetBool ("Characters Screen", false);
+				anim.SetBool ("Options Screen", false);
+				anim.SetBool ("Exit Screen", false);
 			}
 		}
 	}

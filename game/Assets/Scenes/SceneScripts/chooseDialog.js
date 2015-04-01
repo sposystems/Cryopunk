e22684e82ec2d1d93769cfb057ec81b8110b2029
Donal <djterrell@sbcglobal.net>
2015-04-01 08:42:15 -0400

    Protocave
    Last stable version of cave before I broke it messing with scripting.
    Bonus cave textures included.

---
+++ game/Assets/Scenes/SceneScripts/chooseDialog.js
@@ -0,0 +1,41 @@
+﻿var showText : boolean;
+var style: GUIStyle;
+public var text : String;
+var inBox : boolean;
+
+function Update()
+{
+	if(Input.GetKeyDown (KeyCode.Return))
+	{
+		if(inBox)
+		{
+			Time.timeScale = 0;
+			showText = true;
+			inBox = false;
+		}
+		else
+		{
+			Time.timeScale = 1;
+			showText = false;
+		}
+
+	}
+}
+
+function OnTriggerEnter()
+{
+	inBox = true;
+}
+
+function OnTriggerExit()
+{
+	inBox = false;
+}
+
+function OnGUI()
+{
+	if(showText)
+	{
+		GUI.Label(Rect(500,300,200,20), text, style);
+	}
+}
---
+++ game/Assets/Scenes/SceneScripts/playDialog.js
@@ -0,0 +1,31 @@
+﻿var showText : boolean;
+var style: GUIStyle;
+public var text : String;
+var textNotShown : boolean;
+
+function Update()
+{
+	if(Input.GetKeyDown (KeyCode.Return))
+	{
+		Time.timeScale = 1;
+		showText = false;
+		textNotShown = false;
+	}
+}
+
+function OnTriggerEnter()
+{
+	if (textNotShown)
+	{
+		Time.timeScale = 0;
+		showText = true;
+	}
+}
+
+function OnGUI()
+{
+	if(showText)
+	{
+		GUI.Label(Rect(500,300,200,20), text, style);
+	}
+}

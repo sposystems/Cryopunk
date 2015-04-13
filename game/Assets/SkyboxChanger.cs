4538cae7f798787caadc5aca194556ec9c64842e
sposystems <spo@sposystems.com>
2015-04-13 13:21:05 -0400

    Zombie quiz, black clipping bkg
    I ate soooo much candy...

---
+++ game/Assets/SkyboxChanger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxChanger : MonoBehaviour {
+
+	// Use this for initialization
+	public Material skybox;
+
+	void Awake () {
+		//get MainCamera and set the skybox
+
+		RenderSettings.skybox = skybox;	
+	}
+}
